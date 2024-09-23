30fd74bb345100e9c8791d7d7475565a050acd4a
Karabo Motsileng <mtskar040@myuct.ac.za>
2024-09-24 02:04:20 +0300

    Added more methods to the Guest Class

--- Business/Guest.cs
+++ Business/Guest.cs
@@ -15,11 +15,28 @@ namespace BookingSystem.Business
         private Collection<Reservation> reservations; // To store all the reservations made by a guest
         #endregion
 
+        #region Property Methods
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Phone { set; get; }
+        public string Email { set; get; }
+        #endregion
+
         #region Constructors
-        public Guest()
+        public Guest(int id, string name, int phone, string email)
 		{
-            
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
 		}
+        #endregion
+
+        #region Methods
+        public void addReservation()
+        {
+
+        }
         #endregion
     }
 }
