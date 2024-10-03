662201d3459d595a575e5a4598cecfa226db9c99
Karabo Motsileng <mtskar040@myuct.ac.za>
2024-10-03 21:24:49 +0300

    Removed availability attribute from the Rooms table, Define GetAllRooms() method in DB.cs

--- Data/DB.cs
+++ Data/DB.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.SqlServer.Server;
+﻿using BookingSystem.Business;
+using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BookingSystem.Business.Room;
 
 namespace BookingSystem.Data
 {
@@ -31,6 +34,48 @@ namespace BookingSystem.Data
         #region CRUD Methods
 
         // Define method to fetch all rooms belonging to a given hotel (dependent on the hotelID as a foreign key of a room)
+        public Collection<Room> GetAllRooms()
+        {
+            Collection<Room> rooms = new Collection<Room>();
+
+            try
+            {
+                // Define the query to fetch all rooms with reservationID being null (i.e., unreserved rooms)
+                string query = "SELECT * FROM Rooms WHERE ReservationID IS NULL";
+
+                // Initialize the command object with the query and connection
+                command = new SqlCommand(query, connection);
+
+                // Execute the query and store the results in a data reader
+                dataReader = command.ExecuteReader();
+
+                // Iterate through the result set and populate the collection of rooms
+                while (dataReader.Read())
+                {
+                    Room room = new Room
+                    (
+                        Convert.ToInt32(dataReader["roomID"]),
+                        Convert.ToInt32(dataReader["hotelID"]),
+                        Convert.ToInt32(dataReader["reservationID"]),
+                        Convert.ToDecimal(dataReader["dailyRate"]),
+                        (RoomType)Convert.ToInt32(dataReader["RoomType"])
+                    );
+
+                    rooms.Add(room);
+                }
+
+                // Close the reader after reading
+                dataReader.Close();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions (you could log it or throw the exception up to the caller)
+                Console.WriteLine("An error occurred while fetching the rooms: " + ex.Message);
+            }
+
+            return rooms;
+        }
+
 
 
         // Define a method to fetch all guests in the DB
