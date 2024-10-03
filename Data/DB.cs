0707d0b21d02fb46a73b35b013ffd7950af9062c
Karabo Motsileng <mtskar040@myuct.ac.za>
2024-10-03 17:11:49 +0300

    Included the Data folder into the project as it was hidden by the system

--- Data/DB.cs
+++ Data/DB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace BookingSystem.Data
 {
     public class DB
     {
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader dataReader;
+        SqlDataAdapter dataAdapter;
 
     }
 }
