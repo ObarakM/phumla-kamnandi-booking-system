4431f4b350cf931d6fe92fae4a5eab61a4733b3e
dumisxni <dumisanichambela@outlook.com>
2024-10-04 03:41:57 +0200

    Added functionality for the existing or knew guest

--- Presentation/BookingsForm.Designer.cs
+++ Presentation/BookingsForm.Designer.cs
@@ -32,7 +32,7 @@
             this.nameLabel = new System.Windows.Forms.Label();
             this.phoneLabel = new System.Windows.Forms.Label();
             this.emailLabel = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
             this.idTextBox = new System.Windows.Forms.TextBox();
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.phoneTextBox = new System.Windows.Forms.TextBox();
@@ -40,7 +40,9 @@
             this.header = new System.Windows.Forms.Label();
             this.searchButton = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.completeReservationButton = new System.Windows.Forms.Button();
+            this.newGuestRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.existingGuestRadioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // idLabel
@@ -49,7 +51,7 @@
             this.idLabel.BackColor = System.Drawing.Color.Transparent;
             this.idLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.idLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.idLabel.Location = new System.Drawing.Point(231, 257);
+            this.idLabel.Location = new System.Drawing.Point(240, 396);
             this.idLabel.Name = "idLabel";
             this.idLabel.Size = new System.Drawing.Size(37, 28);
             this.idLabel.TabIndex = 14;
@@ -61,7 +63,7 @@
             this.nameLabel.BackColor = System.Drawing.Color.Transparent;
             this.nameLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.nameLabel.Location = new System.Drawing.Point(231, 340);
+            this.nameLabel.Location = new System.Drawing.Point(240, 479);
             this.nameLabel.Name = "nameLabel";
             this.nameLabel.Size = new System.Drawing.Size(78, 28);
             this.nameLabel.TabIndex = 15;
@@ -73,7 +75,7 @@
             this.phoneLabel.BackColor = System.Drawing.Color.Transparent;
             this.phoneLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.phoneLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.phoneLabel.Location = new System.Drawing.Point(231, 398);
+            this.phoneLabel.Location = new System.Drawing.Point(240, 537);
             this.phoneLabel.Name = "phoneLabel";
             this.phoneLabel.Size = new System.Drawing.Size(83, 28);
             this.phoneLabel.TabIndex = 16;
@@ -85,48 +87,48 @@
             this.emailLabel.BackColor = System.Drawing.Color.Transparent;
             this.emailLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.emailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.emailLabel.Location = new System.Drawing.Point(231, 461);
+            this.emailLabel.Location = new System.Drawing.Point(240, 600);
             this.emailLabel.Name = "emailLabel";
             this.emailLabel.Size = new System.Drawing.Size(74, 28);
             this.emailLabel.TabIndex = 17;
             this.emailLabel.Text = "Email";
             // 
-            // label5
+            // headerLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.label5.Location = new System.Drawing.Point(285, 191);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(296, 42);
-            this.label5.TabIndex = 18;
-            this.label5.Text = "Search for Guest";
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.headerLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.headerLabel.Location = new System.Drawing.Point(285, 191);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(361, 42);
+            this.headerLabel.TabIndex = 18;
+            this.headerLabel.Text = "Select Type of Guest";
             // 
             // idTextBox
             // 
-            this.idTextBox.Location = new System.Drawing.Point(404, 264);
+            this.idTextBox.Location = new System.Drawing.Point(413, 403);
             this.idTextBox.Name = "idTextBox";
             this.idTextBox.Size = new System.Drawing.Size(202, 20);
             this.idTextBox.TabIndex = 19;
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(404, 348);
+            this.nameTextBox.Location = new System.Drawing.Point(413, 487);
             this.nameTextBox.Name = "nameTextBox";
             this.nameTextBox.Size = new System.Drawing.Size(332, 20);
             this.nameTextBox.TabIndex = 20;
             // 
             // phoneTextBox
             // 
-            this.phoneTextBox.Location = new System.Drawing.Point(404, 407);
+            this.phoneTextBox.Location = new System.Drawing.Point(413, 546);
             this.phoneTextBox.Name = "phoneTextBox";
             this.phoneTextBox.Size = new System.Drawing.Size(332, 20);
             this.phoneTextBox.TabIndex = 21;
             // 
             // emailTextBox
             // 
-            this.emailTextBox.Location = new System.Drawing.Point(404, 470);
+            this.emailTextBox.Location = new System.Drawing.Point(413, 609);
             this.emailTextBox.Name = "emailTextBox";
             this.emailTextBox.Size = new System.Drawing.Size(332, 20);
             this.emailTextBox.TabIndex = 22;
@@ -150,7 +152,7 @@
             // 
             this.searchButton.BackgroundImage = global::BookingSystem.Properties.Resources.pexels_lilartsy_1194775;
             this.searchButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.searchButton.Location = new System.Drawing.Point(640, 244);
+            this.searchButton.Location = new System.Drawing.Point(649, 383);
             this.searchButton.Name = "searchButton";
             this.searchButton.Size = new System.Drawing.Size(96, 63);
             this.searchButton.TabIndex = 49;
@@ -168,20 +170,48 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button1
+            // completeReservationButton
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImage = global::BookingSystem.Properties.Resources.pexels_soulful_pizza_2080276_3914755__1_;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(499, 509);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 44);
-            this.button1.TabIndex = 23;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.completeReservationButton.BackColor = System.Drawing.Color.Transparent;
+            this.completeReservationButton.BackgroundImage = global::BookingSystem.Properties.Resources.pexels_soulful_pizza_2080276_3914755__1_;
+            this.completeReservationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.completeReservationButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.completeReservationButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.completeReservationButton.Location = new System.Drawing.Point(626, 646);
+            this.completeReservationButton.Name = "completeReservationButton";
+            this.completeReservationButton.Size = new System.Drawing.Size(119, 57);
+            this.completeReservationButton.TabIndex = 23;
+            this.completeReservationButton.Text = "Complete Reservation";
+            this.completeReservationButton.UseVisualStyleBackColor = false;
+            this.completeReservationButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // newGuestRadioButton1
+            // 
+            this.newGuestRadioButton1.AutoSize = true;
+            this.newGuestRadioButton1.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic);
+            this.newGuestRadioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.newGuestRadioButton1.Location = new System.Drawing.Point(245, 286);
+            this.newGuestRadioButton1.Name = "newGuestRadioButton1";
+            this.newGuestRadioButton1.Size = new System.Drawing.Size(154, 32);
+            this.newGuestRadioButton1.TabIndex = 50;
+            this.newGuestRadioButton1.TabStop = true;
+            this.newGuestRadioButton1.Text = "New Guest";
+            this.newGuestRadioButton1.UseVisualStyleBackColor = true;
+            this.newGuestRadioButton1.CheckedChanged += new System.EventHandler(this.newGuestRadioButton1_CheckedChanged);
+            // 
+            // existingGuestRadioButton2
+            // 
+            this.existingGuestRadioButton2.AutoSize = true;
+            this.existingGuestRadioButton2.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Italic);
+            this.existingGuestRadioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.existingGuestRadioButton2.Location = new System.Drawing.Point(548, 286);
+            this.existingGuestRadioButton2.Name = "existingGuestRadioButton2";
+            this.existingGuestRadioButton2.Size = new System.Drawing.Size(197, 32);
+            this.existingGuestRadioButton2.TabIndex = 51;
+            this.existingGuestRadioButton2.TabStop = true;
+            this.existingGuestRadioButton2.Text = "Existing Guest";
+            this.existingGuestRadioButton2.UseVisualStyleBackColor = true;
+            this.existingGuestRadioButton2.CheckedChanged += new System.EventHandler(this.existingGuestRadioButton2_CheckedChanged);
             // 
             // BookingsForm
             // 
@@ -189,15 +219,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1924, 772);
+            this.Controls.Add(this.existingGuestRadioButton2);
+            this.Controls.Add(this.newGuestRadioButton1);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.header);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.completeReservationButton);
             this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.phoneTextBox);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.idTextBox);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.headerLabel);
             this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.phoneLabel);
             this.Controls.Add(this.nameLabel);
@@ -216,14 +248,16 @@
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label phoneLabel;
         private System.Windows.Forms.Label emailLabel;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label headerLabel;
         private System.Windows.Forms.TextBox idTextBox;
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.TextBox phoneTextBox;
         private System.Windows.Forms.TextBox emailTextBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button completeReservationButton;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label header;
         private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.RadioButton newGuestRadioButton1;
+        private System.Windows.Forms.RadioButton existingGuestRadioButton2;
     }
 }
