386cd161f5a860931db0ef81e652cc279c87bd05
dumisxni <dumisanichambela@outlook.com>
2024-10-04 05:19:33 +0200

    reports form for the reports

---
+++ Presentation/ReportsForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookingSystem.Presentation
+{
+    public partial class ReportsForm : Form
+    {
+        public ReportsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            PhumlaKamnandiHotelForm.phumlaKamnandi.Show();
+            this.Close();
+        }
+    }
+}
